fc9a35e0fb50f1e018b58483fb934539a136f100
Lázaro Kabib <lazarokabib94@gmail.com>
2022-07-18 13:10:02 -0300

    fix: trocando "" por

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const ativosRouter = require("./routes/ativos");
+const express = require('express');
+const ativosRouter = require('./routes/ativos');
 
 const app = express();
 
@@ -7,8 +7,8 @@ app.use(express.json());
 
 app.use('/ativos', ativosRouter);
 
-app.get("/", (_req, res) => {
-  res.send("testando");
+app.get('/', (_req, res) => {
+  res.send('testando');
 });
 
 module.exports = app;
--- src/routes/ativos.js
+++ src/routes/ativos.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const ativos = require("../controllers/ativos");
+const express = require('express');
+const ativos = require('../controllers/ativos');
 const router = express.Router();
 
-router.get("/", ativos.getAll);
+router.get('/', ativos.getAll);
 
 module.exports = router;
