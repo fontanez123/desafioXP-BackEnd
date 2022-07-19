13a2863a733fb1389790271443e84c73052b13f7
Lázaro Kabib <lazarokabib94@gmail.com>
2022-07-19 14:51:01 -0300

    feat: adicionando filter para tirar o ativo que o cliente não tem

--- src/services/clientesAtivos.js
+++ src/services/clientesAtivos.js
@@ -1,6 +1,11 @@
 const clientesAtivosModel = require('../models/clientesAtivos');
 
-const clienteAtivos = (idCliente) => clientesAtivosModel.getClienteAtivos(idCliente);
+const clienteAtivos = async (idCliente) => { 
+   const arrayClienteAtivos = await clientesAtivosModel.getClienteAtivos(idCliente);
+   const filter = arrayClienteAtivos.filter((el) => el.quantidade !== 0);
+
+   return filter;
+}
 
 module.exports = {
     clienteAtivos,
