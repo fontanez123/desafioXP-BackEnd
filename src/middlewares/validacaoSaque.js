cf3397c1a4352ea46379fdd64aa738f8c0d801ec
Lázaro Kabib <lazarokabib94@gmail.com>
2022-07-19 11:35:01 -0300

    fix: arrumando bug com a validação de saque

--- src/middlewares/validacaoSaque.js
+++ src/middlewares/validacaoSaque.js
@@ -4,11 +4,6 @@ const validacaoSaque = async (req, res, next) => {
     const { idCliente, valor } = req.body;
     const cliente = await clientesModel.getById(idCliente);
 
-    if(valor > cliente.saldo) {
-        const message = 'Saldo Insuficiente';
-        return res.status(400).json({ message });
-    }
-
     if(valor <= 0) {
         const message = 'O valor do saque não pode ser negativo ou igual a zero.';
         return res.status(400).json({ message });
@@ -19,6 +14,11 @@ const validacaoSaque = async (req, res, next) => {
         return res.status(400).json({ message });
     };
 
+    if(valor > cliente.saldo) {
+        const message = 'Saldo Insuficiente';
+        return res.status(400).json({ message });
+    }
+    
     next();
 };
 
