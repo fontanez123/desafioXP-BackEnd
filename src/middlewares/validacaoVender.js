27cb6aae65ba587abccc83cb442b9d090e24cc36
Lázaro Kabib <lazarokabib94@gmail.com>
2022-07-19 14:00:43 -0300

    feat: implementando validação de quantidade de ativos na rota post /investimentos/vender

---
+++ src/middlewares/validacaoVender.js
@@ -0,0 +1,28 @@
+const clientesAtivosModel = require('../models/clientesAtivos');
+
+const quantidade = async (req, res, next) => {
+    const { idCliente, idAtivo, quantidade } = req.body;
+    const arrayClienteAtivo = await clientesAtivosModel.existeIdsIguais(idCliente, idAtivo);
+
+    if(quantidade > arrayClienteAtivo[0].quantidade) {
+        const message = 'Quantidade de ativo a ser vendida é maior que a quantidade disponível na carteira';
+        return res.status(400).json({ message });
+    };
+
+    if(quantidade <= 0) {
+        const message = 'A quantidade de ativos não pode ser negativa ou igual a zero.';
+        return res.status(400).json({ message });
+    }
+
+    if(typeof quantidade !== 'number') {
+        const message = 'A quantidade precisa ser um número';
+        return res.status(400).json({ message });
+    };
+
+
+    next();
+};
+
+module.exports = {
+    quantidade,
+}
--- src/routes/investimentos.js
+++ src/routes/investimentos.js
@@ -2,6 +2,7 @@ const express = require('express');
 const transacoesAtivos = require('../controllers/transacoesAtivos');
 
 const validacaoComprar = require('../middlewares/validacaoComprar');
+const validacaoVender = require('../middlewares/validacaoVender');
 const validacaoIds = require('../middlewares/validacaoIds');
 
 const router = express.Router();
@@ -16,6 +17,7 @@ transacoesAtivos.comprarAtivos
 router.post('/vender',
 validacaoIds.cliente,
 validacaoIds.ativo,
+validacaoVender.quantidade,
 transacoesAtivos.venderAtivos
 );
 
