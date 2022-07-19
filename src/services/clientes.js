79ceaabe927cff70b3ebdd572635ea100da7fab6
Lázaro Kabib <lazarokabib94@gmail.com>
2022-07-19 16:39:42 -0300

    feat: gerando token JWT pelo login

--- src/services/clientes.js
+++ src/services/clientes.js
@@ -1,11 +1,13 @@
 const clientesModel = require('../models/clientes');
+const { generateJWTTOKEN } = require('../utils/token');
 
 const getById = (id) => clientesModel.getById(id);
 
 const login = async ({ email, senha }) => {
-    const usuario = await clientesModel.login(email, senha);
+    await clientesModel.login(email, senha);
 
-    return usuario;
+    const token = generateJWTTOKEN({ email });
+    return { token };
 };
 
 module.exports = {
