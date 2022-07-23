da20d374dc6dbb7c1d967f7e5ffcf6203e4a5f91
Lázaro Kabib <lazarokabib94@gmail.com>
2022-07-23 16:51:58 -0300

    test: camada service de clientes: testando a função getById

---
+++ src/tests/services/clientes.test.js
@@ -0,0 +1,34 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const clientesModel = require('../../models/clientes');
+const clientesService = require('../../services/clientes');
+
+describe('Testando a camada Service de Clientes', () => {  
+    describe('Testando a função getById', () => {
+        before(async () => {
+          sinon.stub(clientesModel, 'getById')
+            .resolves(
+            { id: 1, nome: 'Lázaro Kabib', saldo: '1033583.20' }
+            );
+        });
+    
+        after(() => {
+          clientesModel.getById.restore();
+        });
+    
+        it('Retorna um objeto', async () => {
+          const result = await clientesService.getById(1);  
+          expect(result).to.be.an('object');
+        });
+    
+        it('O objeto não está vazio', async () => {
+          const result = await clientesService.getById(1);  
+          expect(result).to.be.not.empty;
+        });
+    
+        it('O objeto possui as propriedades: "id", "nome", "saldo"', async () => {
+          const result = await clientesService.getById(1);  
+          expect(result).to.include.all.keys('id', 'nome', 'saldo');
+        });
+    });
+  });
