9da72bdd610ffca1f8b248fe8c77f7c3dc0486ee
Lázaro Kabib <lazarokabib94@gmail.com>
2022-07-23 18:49:21 -0300

    test: camada model de clientes: testando função login

--- src/tests/models/clientes.test.js
+++ src/tests/models/clientes.test.js
@@ -3,6 +3,7 @@ const { expect } = require('chai');
 const clientesModel = require('../../models/clientes');
 
 describe('Testando a camada Model de Clientes', () => {  
+
     describe('Testando a função getById', () => {
         before(async () => {
           sinon.stub(clientesModel, 'getById')
@@ -30,5 +31,33 @@ describe('Testando a camada Model de Clientes', () => {
           expect(result).to.include.all.keys('id', 'nome', 'saldo');
         });
     });
-  });
+
+    describe('Testando a função login', () => {
+      before(async () => {
+        sinon.stub(clientesModel, 'login')
+          .resolves(
+          { email: 'lazarokabib94@gmail.com', senha: 'lazaro1234' }
+          );
+      });
+  
+      after(() => {
+        clientesModel.login.restore();
+      });
+  
+      it('Retorna um objeto', async () => {
+        const result = await clientesModel.login('lazarokabib94@gmail.com', 'lazaro1234');  
+        expect(result).to.be.an('object');
+      });
+  
+      it('O objeto não está vazio', async () => {
+        const result = await clientesModel.login('lazarokabib94@gmail.com', 'lazaro1234');  
+        expect(result).to.be.not.empty;
+      });
+  
+      it('O objeto possui as propriedades: "email", "senha"', async () => {
+        const result = await clientesModel.login('lazarokabib94@gmail.com', 'lazaro1234');  
+        expect(result).to.include.all.keys('email', 'senha');
+      });
+    });
+});
   
