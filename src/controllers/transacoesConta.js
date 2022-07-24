5c3d5629fc43f2f2c34ea126dbe2d7fb723f7a4c
Lázaro Kabib <lazarokabib94@gmail.com>
2022-07-24 17:02:20 -0300

    revert: reverte os status http de deposito, saque, comprar ativos e vender ativos

--- src/controllers/transacoesConta.js
+++ src/controllers/transacoesConta.js
@@ -3,13 +3,13 @@ const transacoesContaService = require('../services/transacoesConta');
 const deposito = async (req, res) => {
   const operacao = await transacoesContaService.deposito(req.body);
 
-  res.status(200).json(operacao);
+  res.status(201).json(operacao);
 };
 
 const saque = async (req, res) => {
   const operacao = await transacoesContaService.saque(req.body);
 
-  res.status(200).json(operacao);
+  res.status(201).json(operacao);
 };
 
 module.exports = {
