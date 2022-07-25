9b99f3282f5d824918ec72a99df232a315b308b0
Lázaro Kabib <lazarokabib94@gmail.com>
2022-07-24 21:36:46 -0300

    fix: colocando return na validação do token e corrigindo bug

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ const auth = (req, res, next) => {
   const token = req.headers.authorization;
 
   if (!token) {
-    res.status(401).json({ message: 'Token não encontrado' });
+    return res.status(401).json({ message: 'Token não encontrado' });
   }
 
   try {
